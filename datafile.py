92c6d5da2460436248a6e4662f39d91b948cb842
Lukepato4 <lukepato4@gmail.com>
2022-05-22 20:24:06 -0500

    Update datafile.py

--- datafile.py
+++ datafile.py
@@ -38,7 +38,7 @@ st.write('---')
 def foo(ticker):
     today = date.today()
 
-    yesterday = today - timedelta(days = 2)
+    yesterday = today - timedelta(days = 3)
 
 
     start = '2022-01-01'      
